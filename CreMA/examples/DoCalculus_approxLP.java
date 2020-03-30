93f95ea86387aae9d927131188050dd3ad153aba
Rafael Cabañas <rcabanas@idsia.ch>
2020-03-30 16:05:13 +0200

    docalculus aproxlp example

--- CreMA/examples/DoCalculus_approxLP.java
+++ CreMA/examples/DoCalculus_approxLP.java
@@ -1,16 +1,18 @@
 import ch.idsia.crema.factor.bayesian.BayesianFactor;
+import ch.idsia.crema.factor.credal.linear.IntervalFactor;
+import ch.idsia.crema.inference.approxlp.Inference;
 import ch.idsia.crema.inference.ve.FactorVariableElimination;
 import ch.idsia.crema.inference.ve.VariableElimination;
 import ch.idsia.crema.model.graphical.SparseDirectedAcyclicGraph;
-import ch.idsia.crema.model.graphical.specialized.BayesianNetwork;
+import ch.idsia.crema.model.graphical.SparseModel;
 import ch.idsia.crema.model.graphical.specialized.StructuralCausalModel;
 import gnu.trove.map.hash.TIntIntHashMap;
 
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
-public class DoCalculus {
-    public static void main(String[] args) {
+public class DoCalculus_approxLP {
+    public static void main(String[] args) throws InterruptedException {
 
     // x <- z -> y ;  x -> y
 
@@ -27,39 +29,41 @@ public class DoCalculus {
         dag.addLink(z,x);
         dag.addLink(z,y);
 
+        // Build the causal model
         StructuralCausalModel smodel = new StructuralCausalModel(dag, endoVarSizes);
         smodel.fillWithRandomFactors(2);
-
-        // Conditioning P( Y | x=0)
-
-        VariableElimination ve = new FactorVariableElimination(smodel.getVariables());
-        ve.setFactors(smodel.getFactors());
-        ve.setEvidence(new TIntIntHashMap(new int[]{x}, new int[]{0}));
-        BayesianFactor pcond = (BayesianFactor)ve.run(y);
-        System.out.println(Arrays.toString(pcond.getData()));
-
-
-        // intervention    do(x=0)
-        StructuralCausalModel do_model = smodel.intervention(x,0);
-
-        // P(Y|do(x=0)) = P'(Y|x=0) =?= P'(Y)
-        ve = new FactorVariableElimination(do_model.getVariables());
-        ve.setFactors(do_model.getFactors());
-        //ve.setEvidence(new TIntIntHashMap(new int[]{x}, new int[]{0}));
-        BayesianFactor pdo = (BayesianFactor)ve.run(y);
-        System.out.println(Arrays.toString(pdo.getData()));
-
-
-        // P(Y|do(x=0)) by operating in the original network
-        System.out.println(Arrays.toString(
-                smodel.getProb(y).combine(smodel.getProb(z)).marginalize(z).filter(x,0).getData()
-        ));
 
 
         BayesianFactor[] factors  = IntStream.of(smodel.getEndogenousVars())
                 .mapToObj(v -> smodel.getProb(v).fixPrecission(5,v))
                 .toArray(BayesianFactor[]::new);
 
-        smodel.toVertexSimple(factors);
+        // Get the equivalent model with CS are defined with constraints///
+        //
+        SparseModel csmodel = smodel.toCredalNetwork(false, factors);
+
+        //SparseModel do_csmodel = csmodel.intervention(x, 0);
+
+
+
+        // Get the equivalent model with CS are defined with constraints
+        //SparseModel csmodel = smodel.toCredalNetwork(false, factors);
+
+
+        // Intervention do(x=0)
+        SparseModel do_csmodel = csmodel.intervention(x, 0);
+
+        // Run inference
+
+        Inference inference = new Inference();
+        IntervalFactor res = inference.query(do_csmodel, y);
+
+        System.out.println(Arrays.toString(res.getUpper()));
+        System.out.println(Arrays.toString(res.getLower()));
+
+
+
+
+
     }
 }
