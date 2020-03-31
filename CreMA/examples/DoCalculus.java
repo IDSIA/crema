1d161f7096b02e81449d7d072b21b21f31f06c30
Rafael Cabañas <rcabanas@idsia.ch>
2020-03-31 20:11:00 +0200

    example with 3 cases

--- CreMA/examples/DoCalculus.java
+++ CreMA/examples/DoCalculus.java
@@ -1,9 +1,14 @@
 import ch.idsia.crema.factor.bayesian.BayesianFactor;
+import ch.idsia.crema.factor.credal.linear.IntervalFactor;
+import ch.idsia.crema.factor.credal.vertex.VertexFactor;
+import ch.idsia.crema.inference.approxlp2.ApproxLP2;
 import ch.idsia.crema.inference.ve.FactorVariableElimination;
 import ch.idsia.crema.inference.ve.VariableElimination;
 import ch.idsia.crema.model.graphical.SparseDirectedAcyclicGraph;
+import ch.idsia.crema.model.graphical.SparseModel;
 import ch.idsia.crema.model.graphical.specialized.BayesianNetwork;
 import ch.idsia.crema.model.graphical.specialized.StructuralCausalModel;
+import gnu.trove.map.TIntIntMap;
 import gnu.trove.map.hash.TIntIntHashMap;
 
 import java.util.Arrays;
@@ -12,54 +17,85 @@ import java.util.stream.IntStream;
 public class DoCalculus {
     public static void main(String[] args) {
 
-    // x <- z -> y ;  x -> y
+        // Marcovian case with 3 endogenous variables  P(Y | do(x=0), z=0)
+        // x <- z -> y ;  x -> y
+
 
         int x=0, y=1, z=2;
         int[] endoVarSizes = {2,2,2};
-
         SparseDirectedAcyclicGraph dag = new SparseDirectedAcyclicGraph();
-
         dag.addVariable(x);
         dag.addVariable(y);
         dag.addVariable(z);
-
         dag.addLink(x,y);
         dag.addLink(z,x);
         dag.addLink(z,y);
 
+        // Build the causal model
         StructuralCausalModel smodel = new StructuralCausalModel(dag, endoVarSizes);
         smodel.fillWithRandomFactors(2);
 
-        // Conditioning P( Y | x=0)
 
-        VariableElimination ve = new FactorVariableElimination(smodel.getVariables());
-        ve.setFactors(smodel.getFactors());
-        ve.setEvidence(new TIntIntHashMap(new int[]{x}, new int[]{0}));
-        BayesianFactor pcond = (BayesianFactor)ve.run(y);
-        System.out.println(Arrays.toString(pcond.getData()));
+        //////// Inputs /////
+
+        StructuralCausalModel model = smodel;
+
+        TIntIntMap evidence = new TIntIntHashMap();
+        evidence.put(z, 0);
+
+        TIntIntMap intervention = new TIntIntHashMap();
+        intervention.put(x, 0);
+
+        int[] target = {y};
+
+        //////////////////
 
 
-        // intervention    do(x=0)
-        StructuralCausalModel do_model = smodel.intervention(x,0);
+        // Case 1: CausalVariableElimination
 
-        // P(Y|do(x=0)) = P'(Y|x=0) =?= P'(Y)
-        ve = new FactorVariableElimination(do_model.getVariables());
+        StructuralCausalModel do_model = model.intervention(intervention.keys()[0], intervention.values()[0]);
+
+        VariableElimination ve = new FactorVariableElimination(do_model.getVariables());
         ve.setFactors(do_model.getFactors());
-        //ve.setEvidence(new TIntIntHashMap(new int[]{x}, new int[]{0}));
-        BayesianFactor pdo = (BayesianFactor)ve.run(y);
-        System.out.println(Arrays.toString(pdo.getData()));
+        ve.setEvidence(evidence);
+        BayesianFactor result_cve = (BayesianFactor) ve.run(target);
+
+        System.out.println(Arrays.toString(result_cve.getData()))
 
 
-        // P(Y|do(x=0)) by operating in the original network
-        System.out.println(Arrays.toString(
-                smodel.getProb(y).combine(smodel.getProb(z)).marginalize(z).filter(x,0).getData()
-        ));
+        // Case 2: CredalCausalVariableElimination
 
-
-        BayesianFactor[] factors  = IntStream.of(smodel.getEndogenousVars())
-                .mapToObj(v -> smodel.getProb(v).fixPrecission(5,v))
+        BayesianFactor[] factors  = IntStream.of(model.getEndogenousVars())
+                .mapToObj(v -> model.getProb(v).fixPrecission(5,v))
                 .toArray(BayesianFactor[]::new);
 
-        smodel.toVertexSimple(factors);
+
+        SparseModel csmodel = smodel.toCredalNetwork(true, factors);
+        SparseModel do_csmodel = csmodel.intervention(intervention.keys()[0], intervention.values()[0]);
+
+
+        ve = new FactorVariableElimination(do_csmodel.getVariables());
+        ve.setFactors(do_csmodel.getFactors());
+        ve.setEvidence(evidence);
+        ve.setNormalize(false);
+        VertexFactor result_ccve = (VertexFactor) ve.run(target);
+
+        System.out.println(result_ccve)
+
+
+        // Case 3: CredalCausalApproxLP
+
+        csmodel = smodel.toCredalNetwork(false, factors);
+        do_csmodel = csmodel.intervention(intervention.keys()[0], intervention.values()[0]);
+
+        ApproxLP2 lp = new ApproxLP2();
+        // IntervalFactor result_ccalp = lp.query(do_csmodel, target[0], evidence) NOT WORKING WITH EVIDENCE
+        IntervalFactor result_ccalp = lp.query(do_csmodel, target[0]);
+        System.out.println(Arrays.toString(result_ccalp.getUpper()));
+        System.out.println(Arrays.toString(result_ccalp.getLower()));
+
+
+
+
     }
 }
