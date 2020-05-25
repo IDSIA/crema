7b26c87c9859550a2facc55a6ef86472af5e493f
Rafael Cabañas <rcabanas@idsia.ch>
2020-05-25 13:48:49 +0000

    update exps

--- CreMA/papers/pgm20/experiments/.ipynb_checkpoints/utilplots-checkpoint.py
+++ CreMA/papers/pgm20/experiments/.ipynb_checkpoints/utilplots-checkpoint.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import math
 import pandas as pd
 
-def plot(data, x_column, y_column, series_column, transformation=None, ax=None, ylabel=None, xlabel=None, title=None):
+def plot(data, x_column, y_column, series_column, transformation=None, ax=None, 
+         ylabel=None, xlabel=None, title=None, legend=True, linecolors = None):
 
     ax = ax or plt.figure(figsize=(10, 8), dpi= 80, facecolor='w', edgecolor='k').gca()
 
@@ -15,13 +16,15 @@ def plot(data, x_column, y_column, series_column, transformation=None, ax=None, 
     
     data = data.replace([np.inf, -np.inf], np.nan).dropna(subset=[y_column])    
     series = np.unique(data[series_column].to_numpy())
-    for s in series:       
+    for s in series:    
+            print(f"{series_column}=='{s}'")
             data_s = data.query(f"{series_column}=='{s}'")
             x = np.unique(data_s[x_column].to_numpy())
             y = data_s.groupby(x_column).mean().filter(items=[y_column]).to_numpy()
 
             ax.plot(x, y, marker='o', label=s)
-            ax.legend()
+            
+            if legend: ax.legend()
 
     ax.set_ylabel(ylabel or y_column)
     ax.set_xlabel(xlabel or x_column)
@@ -65,13 +68,14 @@ def get_rmse_bounds(data):
 ####
 
 def plot_time(data, *args, **kwargs):
-    return plot(data, "N", "time", "method", *args, **kwargs)
+    series_column = "method" if "series_column" not in kwargs else kwargs.pop("series_column")
+    return plot(data, "N", "time", series_column, *args, **kwargs)
 
 def plot_size(data, *args, **kwargs):
     T = [] if "transformation" not in kwargs else kwargs.pop("transformation")
-    return plot(data, "N", "mean_size", "method", transformation = T + [compute_mean_size], *args, **kwargs)
+    series_column = "method" if "series_column" not in kwargs else kwargs.pop("series_column")
+    return plot(data, "N", "mean_size", series_column, transformation = T + [compute_mean_size], *args, **kwargs)
 
 def plot_rmse(data, *args, **kwargs):
     T = [] if "transformation" not in kwargs else kwargs.pop("transformation")
-    return plot(data, "N", "rmse", "method", transformation = T + [get_rmse_bounds], *args, **kwargs)
-
+    return plot(data, "N", "rmse", series_column, transformation = T + [get_rmse_bounds], *args, **kwargs)
