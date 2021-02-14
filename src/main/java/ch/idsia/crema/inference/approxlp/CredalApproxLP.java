a3beffb35cca85e7cefc206fcb386015899ee3d3
Rafael Cabañas <rcabanas@idsia.ch>
2021-02-14 17:21:14 +0100

    conversor fix: #59 and using global random seed

--- src/main/java/ch/idsia/crema/inference/approxlp/CredalApproxLP.java
+++ src/main/java/ch/idsia/crema/inference/approxlp/CredalApproxLP.java
@@ -29,11 +29,16 @@ public class CredalApproxLP<M extends GraphicalModel<? super Factor<?>>> impleme
 	}
 
 	@Override
+	public IntervalFactor query(int target) throws InterruptedException{
+		return query(target, new TIntIntHashMap());
+	}
+
+	@Override
 	public IntervalFactor query(int target, TIntIntMap evidence) throws InterruptedException {
 
 		M infModel = getInferenceModel(target, evidence);
 
-		TIntIntMap filteredEvidence = new TIntIntHashMap(evidence);
+		TIntIntMap filteredEvidence = new TIntIntHashMap();
 
 		// update the evidence
 		for (int v : evidence.keys()) {
