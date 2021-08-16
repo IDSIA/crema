bd46392e3066ff9f0930ed0b5cd6252d9411d6ef
Claudio Bonesana <cbonesana@gmail.com>
2021-08-16 15:34:52 +0200

    Added stub for Bayesian Or Factors.

---
+++ src/main/java/ch/idsia/crema/factor/bayesian/BayesianNoisyOrFactor.java
@@ -0,0 +1,12 @@
+package ch.idsia.crema.factor.bayesian;
+
+/**
+ * Author:  Claudio "Dna" Bonesana
+ * Project: crema
+ * Date:    16.08.2021 15:33
+ */
+public class BayesianNoisyOrFactor {
+
+	// TODO
+
+}
