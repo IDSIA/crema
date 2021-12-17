415929cf55cc306525f40cac146071bf969a7d95
Claudio Bonesana <cbonesana@gmail.com>
2021-12-17 18:46:33 +0100

    Cleanup of cmake files, still working on them.

---
+++ src/main/cpp/crema/version.h
@@ -0,0 +1,7 @@
+#ifndef IDSIA_CREMA_H
+#define IDSIA_CREMA_H
+
+#define Crema_VERSION_MAJOR 0
+#define Crema_VERSION_MINOR 3
+
+#endif // IDSIA_CREMA_H
--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,11 +1,14 @@
-#include "crema/crema.h"
+#include "crema.h"
+#include "Domain.h"
 
-#include <iostream>
+// #include <iostream>
+// #include "boost/graph/astar_search.hpp"
 
 using namespace std;
 using namespace crema;
 
 int main()
 {
+    Domain *d = new Domain();
     cout << "Crema" << endl;
 }
